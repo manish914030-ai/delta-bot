dbaee150b0769b31c807c46943809cbad6061b89
manish914030-ai <manish914030@gmail.com>
2026-08-04 16:31:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from exchange import get_balance
+from telegram_bot import send_message
+
+def main():
+    print("Delta Trading Bot Started")
+
+    try:
+        balance = get_balance()
+        print(balance)
+
+        send_message("✅ Delta Trading Bot Started Successfully")
+
+    except Exception as e:
+        print(e)
+        send_message(f"❌ Bot Error: {e}")
 
 if __name__ == "__main__":
-    print("Delta Trading Bot Started...")
-    print(get_balance())
+    main()
