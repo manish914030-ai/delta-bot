7fed544eae3f87c9176067700726dfd186e01dd4
manish914030-ai <manish914030@gmail.com>
2026-08-04 23:41:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,7 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
 
-API_KEY = os.getenv("DELTA_API_KEY")
-API_SECRET = os.getenv("DELTA_API_SECRET")
-
-SYMBOLS = [
-    "BTCUSDT",
-    "ETHUSDT",
-    "SOLUSDT",
-    "XAUUSD"
-]
-
-TIMEFRAMES = [
-    "5m",
-    "15m",
-    "30m",
-    "1h"
-]
-
-LEVERAGE = 10
-RISK_PERCENT = 2
-ACCOUNT_BALANCE = 80
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
